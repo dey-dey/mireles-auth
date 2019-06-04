fe1a563b926610ea3640c29a16d13c7970172a02
ACJDG <amadeusjunqueira@gmail.com>
2019-06-04 18:26:03 -0400

    Removes JPA entity from role domain object

--- src/main/java/com/deydey/domain/Role.java
+++ src/main/java/com/deydey/domain/Role.java
@@ -5,20 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 
-@Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Role extends Auditable {
+public class Role {
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private long id;
 
-		@Column
 		private String role;
 
 }
