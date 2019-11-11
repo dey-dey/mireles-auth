4539f0ab5f0e655b9e30f75dd8f18ba15e31c62f
ACJDG <amadeusjunqueira@gmail.com>
2019-11-11 17:14:02 -0600

    Update sql table composition

--- src/main/resources/db/migration/V4__add_login.sql
+++ src/main/resources/db/migration/V4__add_login.sql
@@ -1,7 +1,7 @@
-CREATE TABLE login
+CREATE TABLE authentication
 (
     id bigserial NOT NULL,
-    membership_id      bigint       NOT NULL references membership on delete cascade,
+    member_id      bigint       NOT NULL references member on delete cascade,
     active             boolean      not null,
     password_hash      varchar(255) NOT NULL,
     password_algorithm varchar(255) NOT NULL,
@@ -11,10 +11,10 @@ CREATE TABLE login
     PRIMARY KEY (id)
 );
 
-CREATE TABLE login_attempt
+CREATE TABLE authentication_attempt
 (
     id bigserial NOT NULL,
-    login_id   bigint       NOT NULL references login on delete cascade,
+    authentication_information_id bigint NOT NULL references authentication on delete cascade,
     successful varchar(36)  NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     user_agent varchar(255) NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE login_attempt
 CREATE TABLE token
 (
     id bigserial NOT NULL,
-    membership_id bigint NOT NULL references membership on delete cascade,
+    member_id bigint NOT NULL references member on delete cascade,
     type          VARCHAR(120) NOT NULL,
     token         varchar(36)  NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
@@ -35,7 +35,7 @@ CREATE TABLE token
 CREATE TABLE token_confirmation
 (
     id bigserial  NOT NULL,
-    membership_id bigint NOT NULL references membership on delete cascade,
+    member_id bigint NOT NULL references member on delete cascade,
     type          varchar(120) NOT NULL,
     token         varchar(36)  NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
