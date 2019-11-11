58e5a08eaf92995f8d278799848281a12d1d7d21
ACJDG <amadeusjunqueira@gmail.com>
2019-11-11 16:58:58 -0600

    Clean up application constructor

--- src/main/java/com/deydey/Oauth2JwtAuthenticationApplication.java
+++ src/main/java/com/deydey/Oauth2JwtAuthenticationApplication.java
@@ -2,18 +2,10 @@ package com.deydey;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Oauth2JwtAuthenticationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2JwtAuthenticationApplication.class, args);
-	}
-
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
