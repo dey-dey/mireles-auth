381435d738ec72dc88e8e9285a37e4022db9b908
ACJDG <amadeusjunqueira@gmail.com>
2019-11-14 17:03:14 -0600

    fixing registration int test

--- src/test-integration/groovy/com/deydey/iam/application/service/RegistrationServiceIntTest.groovy
+++ src/test-integration/groovy/com/deydey/iam/application/service/RegistrationServiceIntTest.groovy
@@ -2,20 +2,21 @@ package com.deydey.iam.application.service
 
 import com.deydey.common.infrastructure.spring.ApplicationConfig
 import com.deydey.configuration.DatabaseIntegrationTest
+import com.deydey.iam.api.dto.RegistrationDto
 import com.deydey.iam.application.command.registration.CreateRegistrationCommand
 import com.deydey.iam.domain.access.authorization.RoleService
 import com.deydey.iam.domain.identity.tenant.TenantRepository
+import com.deydey.iam.domain.identity.user.Member
+import com.deydey.iam.domain.identity.user.MemberId
 import com.deydey.iam.domain.identity.user.MemberRepository
 import com.deydey.iam.domain.identity.user.User
+import com.deydey.iam.domain.identity.user.UserId
 import com.deydey.iam.domain.identity.user.UserRepository
 import com.deydey.iam.infrastructure.persistence.jdbc.repositories.RoleJdbcRepository
-import com.deydey.iam.infrastructure.persistence.jdbc.repositories.TenantJdbcRepository
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
-import javax.sql.DataSource
 
 @Slf4j
 class RegistrationServiceIntTest extends DatabaseIntegrationTest {
@@ -46,16 +47,18 @@ class RegistrationServiceIntTest extends DatabaseIntegrationTest {
 
     def "registers a personal admin" () {
         given:
+            def expectedEmail = "yo@yo.com"
             CreateRegistrationCommand createRegistrationCommand = CreateRegistrationCommand.builder()
-                .email("yo@yo.com")
+                .email(expectedEmail)
                 .password("yo")
                 .build()
         when:
-            User actualResult = objectUnderTest.registerUserAsTenant(createRegistrationCommand).get()
-            User savedUser = userRepository.getUserBy(actualResult.getId()).get()
+            RegistrationDto actualResult = objectUnderTest.registerUserAsTenant(createRegistrationCommand)
+            User savedUser = userRepository.getBy(new UserId(actualResult.getUserId())).get()
+            Member savedMember = memberRepository.getBy(new MemberId(actualResult.getMemberId()))
         then:
-            log.info("actualResult.getId() ${actualResult.getId().value} ${savedUser.getId().value}")
             actualResult.getId() == savedUser.getId()
-//            savedUser.userIdentityInformation.defaultEmail == "yo@yo.com"
+            actualResult.getTenantId() == savedMember.getTenantId().getValue()
+            actualResult.getEmail() == expectedEmail
     }
 }
