5e7d8dfa7467575bc7eb8bc08ed51aed8a39ccb5
ACJDG <amadeusjunqueira@gmail.com>
2019-11-27 17:02:39 -0500

    replace old migration name with appropriate name

--- src/main/resources/db/migration/V3__add_accesses.sql
+++ src/main/resources/db/migration/V3__add_accesses.sql
@@ -2,7 +2,9 @@ CREATE TABLE role
 (
     id bigserial NOT NULL,
     role VARCHAR(120) NOT NULL,
+    role_description VARCHAR(120) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     PRIMARY KEY (id)
 );
 
