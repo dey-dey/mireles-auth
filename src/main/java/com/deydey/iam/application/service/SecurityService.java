b0f69eb019ea7227548677fafefc657c4e15a8bc
Amadeus J <amadeusjunqueira@gmail.com>
2020-05-29 17:03:52 -0400

    register beans with spring stereotypes

--- src/main/java/com/deydey/iam/application/service/SecurityService.java
+++ src/main/java/com/deydey/iam/application/service/SecurityService.java
@@ -2,10 +2,12 @@ package com.deydey.iam.application.service;
 
 import com.deydey.iam.domain.access.authorization.Role;
 import com.deydey.iam.domain.identity.user.Member;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class SecurityService {
 	public List<Role> findRolesForMember(Member member) {
 		return new ArrayList<>();
